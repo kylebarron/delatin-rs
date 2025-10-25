3774cec296bf8723f44c402d82000f7f6a9c08a7
Kyle Barron <kyle@developmentseed.org>
2025-10-24 23:20:04 -0400

    Vendor error enum

---
+++ src/error.rs
@@ -0,0 +1,11 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum TriangulationError {
+    #[error("No max error in queue.")]
+    MaxErrorRetrievalError,
+    #[error("Priority queue is empty.")]
+    EmptyQueueError,
+    #[error("Length of heights data is not equal to width * height.")]
+    InvalidDataLengthError,
+}
